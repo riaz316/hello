420d55b55f4e3eb56bd5e0efe14a20ecb393adb4
Mohammad Riaz <mdriaz0407@gmail.com>
2024-01-08 01:31:02 +0000

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,1 @@
-pprint("Epoch 1/5/n144/144 [==============================] - 302s 2s/step - loss: 0.5518 - accuracy: 0.7236 - val_loss: 0.5514 - val_accuracy: 0.7647")
+print("Epoch 1/5/n144/144 [==============================] - 302s 2s/step - loss: 0.5518 - accuracy: 0.7236 - val_loss: 0.5514 - val_accuracy: 0.7647")
